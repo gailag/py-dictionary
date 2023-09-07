8ec57b80b2670378699c43d7e2e9c4c174c73aa8
Gaila Galano <gailag@hawaii.edu>
2023-09-07 09:23:22 -0400

    Update guide.py

--- guide.py
+++ guide.py
@@ -4,12 +4,32 @@
 
 #tuples VS list
 # tuples cannot be modified. Used to represent a fixed collection of items
-# list can be modified. Used as a reference to calculate something
+# list can be modified
 
 #Tuples: cannot be modified
 tuple = ("apple", "banana", "cherry")
 
-#List
+#List: ordered, changeable, and allow duplicate values
+list = ["notebook", "pencil", "eraser"]
 
 #Arrays: used to store multiple values in one single var
+#cars[0] = Ford
 cars = ["Ford", "Volvo", "BMW"]
+
+#Operators: perform operations on var and values
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
+print(10 % 5)
+print(10 ** 5)
+print(10 // 5)
+
+#Set: stores multiple items in a single var.
+#unchangeable, but can remove items and add new items
+set = {"burger", "fries", "cola"}
+
+#Lambda: anonymous function
+#multiplication
+x = lambda a, b : a * b
+print(x(5, 6))
