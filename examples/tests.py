11a73a9d94303e36fef150ec8c126631375a98ab
Gaila Galano <76193969+gailag@users.noreply.github.com>
2024-01-13 22:02:23 -0500

    Create tests.py

---
+++ examples/tests.py
@@ -0,0 +1,19 @@
+#Q1: Given a number n, return all numbers less than n that are even
+#ex: isEven(10) -> [0,2,4,6,8]
+
+def isEven(n:int) -> List[int]
+
+
+#Q2: Given a list of numbers, return all non-repeating numbers, if all are repeating, return 0
+#Expected output is a list
+#E.g. [1,1,2,2,3] -> [3]
+#[1,2,2] -> [0]
+
+def returnSingleNumber(l:List[int]) -> List[int]
+
+
+#Q3: Given an arbitrary length credit card number, mask all characters but the last four digits with *
+#Can assume the string is only represented by digits
+#E.g. 1234567890 -> ******7890
+
+def mask_cc_numbers(cc_string:str) -> str:
