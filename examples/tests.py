a0dfab030df1494f6a11ab93aa6df206d438c506
Gaila Galano <76193969+gailag@users.noreply.github.com>
2024-01-14 22:08:10 -0600

    Update tests.py

--- examples/tests.py
+++ examples/tests.py
@@ -1,8 +1,14 @@
 #Q1: Given a number n, return all numbers less than n that are even
 #ex: isEven(10) -> [0,2,4,6,8]
 
-def isEven(n:int) -> List[int]
+#def isEven(n:int) -> List[int]
 
+list = [2, 4, 6, 10, 22, 48, 50]
+
+for num in list:
+    if num % 2 == 0:
+        print(num)
+        
 
 #Q2: Given a list of numbers, return all non-repeating numbers, if all are repeating, return 0
 #Expected output is a list
